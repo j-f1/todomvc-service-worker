12edf18920de42d1bc982d4dfdd1e9b415088063
Jed Fox <git@twopointzero.us>
2020-04-12 16:10:16 -0400

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -140,7 +140,7 @@ self.addEventListener("fetch", (event) => {
 		event.respondWith(
 			caches.match(event.request).then((res) => res || fetch(event.request))
 		);
-		evt.waitUntil(
+		event.waitUntil(
 			caches
 				.open(CACHE)
 				.then((cache) =>
